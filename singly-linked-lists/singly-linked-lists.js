14352fa86a450eb6531fbf8d8fe70ca53aab065c
Kuddleman <edl1155@gmail.com>
2020-06-22 16:53:25 -0700

    complete push method on single linked lists

--- singly-linked-lists/singly-linked-lists.js
+++ singly-linked-lists/singly-linked-lists.js
@@ -12,5 +12,28 @@ class SinglyLinkedList {
     this.length = 0
   }
 
-  
+  push( val ) {
+    //create new node:
+    const newNode = new Node( val )
+    //if the linked list is empty, set the new value to be the head and the tail:
+    if (!this.head) {
+      this.head = newNode
+      this.tail = this.head
+    } else {
+      //add the new element to the next element after the current tail
+      this.tail.next = newNode
+      //update tail:
+      this.tail = newNode
+    }
+    //make the length 1 longer:
+    this.length++
+    //return the list:
+    return this 
+  }
 }
+
+
+const list = new SinglyLinkedList()
+list.push('hi')
+list.push('you')
+console.log(list)
