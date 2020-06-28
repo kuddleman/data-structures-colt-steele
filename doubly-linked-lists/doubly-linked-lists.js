ff1040e5c236ff94a29e2dbec808dc60452f9c7d
Kuddleman <edl1155@gmail.com>
2020-06-28 12:22:05 -0700

    add pseudocode to implement pushmethod on doubly linked lists

--- doubly-linked-lists/doubly-linked-lists.js
+++ doubly-linked-lists/doubly-linked-lists.js
@@ -13,4 +13,18 @@ class DoublyLinkedList {
     this.tail = null
     this.length = 0
   }
+   
+  // Pseudo code to implement push
+  //create a new node with the value passed in.
+  // check if list is empty. If so, then set the head and the tail
+     // to be the newly created node.
+  //if not, set the next property on the tail to be that node
+    // Set the previous property on the newly created node to be the tail  
+  // Set the tail to be the newly created node
+  // Increment the length
+  //Return the Doubly Linked List     
+  
+  push() {
+
+  }
 }
